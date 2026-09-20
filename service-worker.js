6914a025bda288b6b2b2196bd448948cda9809ea
yyisrealannie <yyisrealannie@gmail.com>
2026-09-20 23:08:50 +0100

    Refine immersive interface: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'loveubae-v15';
+const CACHE_NAME = 'loveubae-v16';
 const LOCAL_ASSETS = [
   './', './index.html', './manifest.webmanifest', './css/styles.css',
   './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png',
